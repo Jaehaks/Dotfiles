3de69b962f44ae14325efe09849ca5ba384b717a
Jaehaks <wogkr3654@naver.com>
2024-06-02 17:46:07 +0900

    feat(fzff) : file open along to file extension

--- clink/fzff.cmd
+++ clink/fzff.cmd
@@ -1,4 +1,26 @@
+:: You must register ms_office and Acrobat in %PATH%
 @echo off
 fzf --layout=reverse --border --preview="bat {}" --bind=ctrl-j:up,ctrl-k:down> %TMP%\fzff.txt
 iconv -f utf-8 -t cp949 %TMP%\fzff.txt > %TMP%\fzff_cp949.txt
-for /f "delims=" %%i in (%TMP%\fzff_cp949.txt) do nvim %%i
+for /f "delims=" %%i in (%TMP%\fzff_cp949.txt) do (
+	for %%e in (.xlsx;.xls;.csv;.xlsm) do (
+		if "%%~xi" == "%%~e" (
+			start excel ^"%%i^"
+			goto :end
+		)
+	)
+	for %%e in (.pptx;.ppt) do (
+		if "%%~xi" == "%%~e" (
+			start powerpnt ^"%%i^"
+			goto :end
+		)
+	)
+	for %%e in (.pdf) do (
+		if "%%~xi" == "%%~e" (
+			start Acrobat ^"%%i^"
+			goto :end
+		)
+	)
+	nvim-qt ^"%%~i^"
+)
+:end
