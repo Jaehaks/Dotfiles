f504389c1741c6b6dfe276a8d93c8a176b894916
Jaehaks <wogkr3654@naver.com>
2024-06-02 17:46:31 +0900

    feat(fzfd) : supports directory as argument

--- clink/fzfd.cmd
+++ clink/fzfd.cmd
@@ -1,4 +1,5 @@
+:: you can give a path as argument
 @echo off
-fd --hidden --type directory . | fzf --layout=reverse --border --bind=ctrl-j:up,ctrl-k:down > %TMP%\fzfd.txt
+fd --hidden --type directory . %1 | fzf --layout=reverse --border --bind=ctrl-j:up,ctrl-k:down > %TMP%\fzfd.txt
 iconv -f utf-8 -t cp949 %TMP%\fzfd.txt > %TMP%\fzfd_cp949.txt
-for /f "delims=" %%i in (%TMP%\fzfd_cp949.txt) do cd %%i
+for /f "delims=" %%i in (%TMP%\fzfd_cp949.txt) do cd /d %%i
