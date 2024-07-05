3d045309af9221ecc1f51a92cf1c28972a75b91a
Jaehaks <wogkr3654@naver.com>
2024-07-05 20:01:57 +0900

    feat(aliase) : use lfcd as lf / add ll instead of ls

--- clink/aliase.cmd
+++ clink/aliase.cmd
@@ -1,7 +1,9 @@
 @echo off
-doskey ls=ls -al --color=always $*
+doskey ls=ls --color=always $*
+doskey ll=ls -al --color=always $*
 doskey grep=grep -i --color=always $*
 doskey zi=z -I $*
 doskey zl=z -l $*
 doskey zt=z -t $*
+doskey lf=lfcd $*
 @echo on
