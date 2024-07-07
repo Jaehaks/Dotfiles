db6ec61b573cc99169ad8280e4ecaff9a87e1efb
Jaehaks <wogkr3654@naver.com>
2024-07-07 17:11:09 +0900

    feat(aliase) : modifiy 'll' for eza

--- clink/aliase.cmd
+++ clink/aliase.cmd
@@ -1,7 +1,8 @@
 @echo off
 doskey ls=ls --color=always $*
-doskey ll=ls -al --color=always $*
-doskey grep=grep -i --color=always $*
+REM doskey ll=ls -al --color=always $*
+doskey ll=eza -al --icons=auto --sort=extension --time-style "+%%Y-%%m-%%d %%H:%%M:%%S" $*
+doskey grep=grep -i --color=auto $*
 doskey zi=z -I $*
 doskey zl=z -l $*
 doskey zt=z -t $*
