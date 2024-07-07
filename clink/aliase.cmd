9beca0f6319190eab36476d00e9c581ce0990aab
Jaehaks <wogkr3654@naver.com>
2024-07-07 23:01:10 +0900

    feat(aliase) : add colorset for eza, date and other extensions

--- clink/aliase.cmd
+++ clink/aliase.cmd
@@ -1,6 +1,9 @@
 @echo off
+REM set EZA_COLOR for date(da)  [you must don't use double quotes]
+set EZA_COLORS=da=38;2;90;241;188:*.pdf=38;2;255;100;100:*.pptx=38;2;255;150;100:*.PPTX=38;2;255;150;100:*.PPT=38;2;255;150;100:*.ppt=38;2;255;150;100:*.xlsx=38;2;200;255;200:*.xls=38;2;200;255;200:*.csv=38;2;200;255;200:*.lnk=38;2;255;255;150:
+
+
 doskey ls=ls --color=always $*
-REM doskey ll=ls -al --color=always $*
 doskey ll=eza -al --icons=auto --sort=extension --time-style "+%%Y-%%m-%%d %%H:%%M:%%S" $*
 doskey grep=grep -i --color=auto $*
 doskey zi=z -I $*
