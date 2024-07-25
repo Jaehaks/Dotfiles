f9ff309680699a72c3c23a2c2bb1fcc265897b1d
Jaehaks <wogkr3654@naver.com>
2024-07-25 11:14:20 +0900

    feat(aliase) : add z.lua options
    _ZL_ADD_ONCE : add score only when I change directory
    _ZL_MATCH_MODE : enhanced mode, detect basename directory first which matches with
    keyword
    _ZL_NO_CHECK=1 : don't filtering undesired named of directory
    		I set this option for non-english named directory,
    		but it doesn't work because from cmd to lua file
    		non-english argument cannot be transfered

--- clink/aliase.cmd
+++ clink/aliase.cmd
@@ -1,6 +1,9 @@
 @echo off
 REM set EZA_COLOR for date(da)  [you must don't use double quotes]
 set EZA_COLORS=da=38;2;90;241;188:*.pdf=38;2;255;100;100:*.pptx=38;2;255;150;100:*.PPTX=38;2;255;150;100:*.PPT=38;2;255;150;100:*.ppt=38;2;255;150;100:*.xlsx=38;2;200;255;200:*.xls=38;2;200;255;200:*.csv=38;2;200;255;200:*.lnk=38;2;255;255;150:
+set _ZL_ADD_ONCE=1
+set _ZL_MATCH_MODE=1
+set _ZL_NO_CHECK=1
 
 
 doskey ls=ls --color=always $*
