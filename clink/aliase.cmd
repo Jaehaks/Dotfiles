c10ecb5ca77563574a6bb5f6044d44530ebc0e45
Jaehaks <wogkr3654@naver.com>
2024-07-25 11:18:49 +0900

    feat(aliase) : add luarocks config to use luautf8 and lua-iconv

--- clink/aliase.cmd
+++ clink/aliase.cmd
@@ -1,4 +1,12 @@
 @echo off
+REM set luarocks config for install library from luarocks
+REM 1) for luautf8
+luarocks config variables.LUA_INCDIR %USERPROFILE%\scoop\apps\lua\current\include > nul
+REM 2) for lua-iconv
+luarocks config external_deps_dirs[2] %USERPROFILE%\scoop\apps\mingw\current\x86_64-w64-mingw32 > nul
+set LUA_CPATH=%USERPROFILE%\scoop\persist\luarocks\rocks\lib\lua\5.4\?.dll;%LUA_CPATH%
+
+
 REM set EZA_COLOR for date(da)  [you must don't use double quotes]
 set EZA_COLORS=da=38;2;90;241;188:*.pdf=38;2;255;100;100:*.pptx=38;2;255;150;100:*.PPTX=38;2;255;150;100:*.PPT=38;2;255;150;100:*.ppt=38;2;255;150;100:*.xlsx=38;2;200;255;200:*.xls=38;2;200;255;200:*.csv=38;2;200;255;200:*.lnk=38;2;255;255;150:
 set _ZL_ADD_ONCE=1
