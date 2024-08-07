fa6fdea080ff001836375e8d71894e26b7607a66
Jaehaks <wogkr3654@naver.com>
2024-08-07 23:02:25 +0900

    feat(aliase) : add env to use zoxide

--- clink/aliase.cmd
+++ clink/aliase.cmd
@@ -9,16 +9,21 @@ set LUA_CPATH=%USERPROFILE%\scoop\persist\luarocks\rocks\lib\lua\5.4\?.dll;%LUA_
 
 REM set EZA_COLOR for date(da)  [you must don't use double quotes]
 set EZA_COLORS=da=38;2;90;241;188:*.pdf=38;2;255;100;100:*.pptx=38;2;255;150;100:*.PPTX=38;2;255;150;100:*.PPT=38;2;255;150;100:*.ppt=38;2;255;150;100:*.xlsx=38;2;200;255;200:*.xls=38;2;200;255;200:*.csv=38;2;200;255;200:*.lnk=38;2;255;255;150:
+
+REM set environment variables for z.lua
 set _ZL_ADD_ONCE=1
 set _ZL_MATCH_MODE=1
 set _ZL_NO_CHECK=1
+
+REM set environment variables for zoxide
+set _ZO_DATA_DIR=%HOME%
 
 
 doskey ls=ls --color=always $*
 doskey ll=eza -al --icons=auto --sort=extension --time-style "+%%Y-%%m-%%d %%H:%%M:%%S" $*
 doskey grep=grep -i --color=auto $*
-doskey zi=z -I $*
-doskey zl=z -l $*
-doskey zt=z -t $*
+REM doskey zi=z -I $*
+REM doskey zl=z -l $*
+REM doskey zt=z -t $*
 doskey lf=lfcd $*
 @echo on
