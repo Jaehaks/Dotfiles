c16b108b3611e763ddd3640f00b58b6624fb69fd
Jaehaks <wogkr3654@naver.com>
2024-11-10 09:27:37 +0900

    feat(fg) : add command to integrated fzf and rg
    - `fg.cmd` : it open fzf to search contents not filename
    - `bat_range.cmd` : it limits range of contents which is shown by bat
    		    it is used by fg.cmd as previewer
    
    - `fg.cmd` can accepts arguments as rg options
    - `fg -t txt -t md` means that finding only *.txt and *.md extension
      option is added above default configuration.
      you can add any rg options like this.

---
+++ clink/fg.cmd
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM get parameters as rg options
+set "rg_options="
+:loop
+if "%~1"=="" goto endloop
+set "rg_options=!rg_options! %~1"
+shift
+goto loop
+:endloop
+
+REM set default rg_options
+set "RG_PREFIX=rg --column --line-number --no-heading --color=always --sort=path --smart-case !rg_options!"
+
+REM do fzf using rg
+for /f "tokens=*" %%a in ('fzf --ansi --disabled --print-query ^
+--bind "start:reload:!RG_PREFIX! {q} || echo." ^
+--bind "change:reload:!RG_PREFIX! {q} " ^
+--delimiter ":" ^
+--preview "bat_range {1} {2} {2}-5 {2}+5" ^
+--preview-window "down,12" ^
+') do (
+	set "result=%%a"
+)
+
+echo !result!
+
+endlocal
