238cbf040dd3a11b999591ab01a2ef0f16ceeaf4
Jaehaks <wogkr3654@naver.com>
2024-11-10 10:01:24 +0900

    feat(fg_fzf) : add command to fuzzy search in all rg result
    fg.cmd's query is rg's input. so it doesn't support fuzzy search
    fg_fzf supports fuzzy search from rg search I think it seems lower
    performance

---
+++ clink/fg_fzf.cmd
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM get parameters as rg options
+set "rg_options="
+:loop
+if "%~1"=="" goto endloop
+set "rg_options=!rg_options! %~1"
+shift
+goto loop
+:endloop
+
+REM set default rg_options
+set "RG_PREFIX=rg --column --line-number --no-heading --color=always --sort=path --smart-case !rg_options!"
+
+REM do fzf using rg
+for /f "tokens=*" %%a in ('!RG_PREFIX! "" ^| fzf --ansi ^
+--delimiter ":" ^
+--preview "bat_range {1} {2} {2}-5 {2}+5" ^
+--preview-window "down,12" ^
+') do (
+	set "result=%%a"
+)
+
+if defined result (
+	echo !result!
+)
+
+endlocal
