2d3be61570afad794700747e8ebcd46252ecd1eb
Jaehaks <wogkr3654@naver.com>
2024-11-13 16:38:58 +0900

    fix(yazi) : add additional extension for mime-ext

--- yazi/init.lua
+++ yazi/init.lua
@@ -53,3 +53,48 @@ require('githead'):setup()
 -- [plugins] - yazi-rs/git.yazi
 -- show git status right after directory
 require('git'):setup()
+
+
+-- [plugins] - yazi-rs/mime-ext.yazi
+-- prefetech file's mime using file extension
+require('mime-ext'):setup({
+	-- by filename database
+	with_files = {
+
+	},
+	-- by extension database
+	with_exts = {
+		-- for matlab
+		m      = "text/matlab",
+		mat    = "application/x-matlab-data",
+		mlx    = "application/matlab",
+		mdl    = "application/matlab",
+		tlc    = "text/x-c",
+		slx    = "application/matlab",
+		slxc   = "application/matlab",
+		mexw64 = "application/vnd.microsoft.portable-executable",
+
+		-- for inca
+		dat   = "application/inca",
+		mf4   = "application/inca",
+		dxl   = "text/inca",
+		ascii = "text/inca",
+
+		-- for code
+		a2l  = "text/plain",
+		hex  = "text/plain",
+		temp = "text/plain",
+		cmm  = "text/plain",
+		bak  = "text/plain",
+		cmd  = "text/plain",
+		bat  = "text/plain",
+		reg  = "text/x-ms-regedit",
+
+		-- for can
+		dbc = "text/plain",
+
+		-- for email
+		pst = "application/outlook",
+	},
+	fallback_file1 = false,
+})
