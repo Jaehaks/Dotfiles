c5ec1ec3961a0b2c403a0233df04eb3479c5d0e1
Jaehaks <wogkr3654@naver.com>
2024-11-17 11:41:46 +0900

    fix(log_fzf) : change timestamp format & theme

--- clink/log_fzf.cmd
+++ clink/log_fzf.cmd
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 REM Define the git log command
-set git_log_cmd=git log --all --color=always --format="%%C(auto)%%h%%d %%s %%C(black)%%C(bold)%%cr"
+set git_log_cmd=git log --all --color=always --format="%%C(auto)%%h%%d %%s %%C(brightcyan)%%cs"
 
 REM Define the fzf command
 set fzf_cmd=^
@@ -12,7 +12,7 @@ fzf ^
 --reverse ^
 --bind "ctrl-s:toggle-sort" ^
 --tiebreak=index ^
---preview "git show -p {1} | bat --color=always --style=header,grid --language=diff" ^
+--preview "git show -p {1} | bat --color=always --style=header,grid --language=diff --theme=ansi" ^
 --preview-window=bottom,60%%
 
 :: Combine the commands
