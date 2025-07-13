8642dd2f7a3f05088335d495f69a9fdde77c9a8a
Jaehaks(arch) <wogkr3654@naver.com>
2025-07-13 15:59:57 +0900

    fix(zsh-arch) : add git status comment to prompt setting

--- zsh/arch/rc.d/05_prompt.zsh
+++ zsh/arch/rc.d/05_prompt.zsh
@@ -1,8 +1,12 @@
 ## for git branch
 autoload -Uz vcs_info
-precmd() { vcs_info }
-zstyle ':vcs_info:git:*' formats '%F{magenta}%b%f'
+# git status --porcelain=v2 --branch --ignored=matching --show-stash
+# https://medium.com/@swindlers-inc/vcs-info-on-zsh-prompt-cfa9629fcd82
 setopt PROMPT_SUBST
+zstyle ':vcs_info:git:*' check-for-changes true # activate confirm changes
+zstyle ':vcs_info:git:*' formats '%F{magenta}%b%f'
+
+precmd() { vcs_info } # executes vcs_info() before prompt
 
 ## final prompt
 PROMPT='%F{yellow}%D{%Y/%m/%d} %D{%H:%M:%S}%f %F{green}%~%f ${vcs_info_msg_0_}
