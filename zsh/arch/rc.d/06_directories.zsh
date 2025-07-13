45e327d3540d46ceeaee1944c617e27b44eb68ee
Jaehaks(arch) <wogkr3654@naver.com>
2025-07-13 12:00:14 +0900

    fix(zsh-arch) : use LOCALOPT/LOCALBIN to set useful directory
    `$LOCALBIN` : To add link which is not include in /usr/bin
    `$LOCALOPT` : To install big programs which is installed by external
    method

--- zsh/arch/rc.d/06_directories.zsh
+++ zsh/arch/rc.d/06_directories.zsh
@@ -1,9 +1,9 @@
 ## directories which must be created
 # caution: use $HOME instead of '~'
 directories=(
-	"$HOME/.plugins"
 	"$HOME/.config"
 	"$HOME/.local/bin"
+	"$HOME/.local/opt"
 	"$HOME/Obsidian_Nvim/Personal" # for neovim
 	"$HOME/Obsidian_Nvim/Project" # form neovim
 )
@@ -12,3 +12,6 @@ for dir in "${directories[@]}"; do
 		mkdir -p "$dir"
 	fi
 done
+
+export LOCALBIN="$HOME/.local/bin"
+export LOCALOPT="$HOME/.local/opt"
