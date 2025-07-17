09fdfcb48dbd9287ff959f4fbf17e8f18c54f1dc
Jaehaks(arch) <wogkr3654@naver.com>
2025-07-17 10:20:51 +0900

    fix(zsh-arch) : load keymaps.zsh at last
    some plugins change keymaps automatically. so manual keymapping needs to
    load lately

--- zsh/arch/rc.d/99_keymaps.zsh
+++ zsh/arch/rc.d/99_keymaps.zsh
@@ -3,4 +3,7 @@
 bindkey -e # set emacs keybinding even thought EDITOR=vi
 bindkey -r "^S" # remove existing keymap (history-incremental-search-forward)
 bindkey "^T" history-incremental-search-forward
+bindkey "^O" clear-screen
+bindkey "^H" backward-char
+bindkey "^L" forward-char
 
