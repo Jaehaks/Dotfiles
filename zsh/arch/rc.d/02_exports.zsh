241c3dca884607631af11be55ff46b36160caf3e
Jaehaks(arch) <wogkr3654@naver.com>
2025-07-19 09:57:06 +0900

    fix(zsh-arch) : fix keymap setting order to expected behavior
    1. Problem
    
    manual keymap setting of zsh-autosuggestions doesn't work because
    `bindkey -e` called after the plugin initiation.
    It resets all keymaps.
    
    2. Solution
    `bindkey -e` called first over any configuration and set keymaps after then.
    Manual keymaps must be called after plugin setup

--- zsh/arch/rc.d/02_exports.zsh
+++ zsh/arch/rc.d/02_exports.zsh
@@ -10,6 +10,8 @@ export XDG_CONFIG_HOME="$HOME/.config"
 
 ## editor
 export EDITOR="nvim"
+bindkey -e # set emacs keybinding even thought EDITOR=vi
+		   # it must be called before other bindkey setting
 
 ## timezone
 export TZ='Asia/Seoul' #timezome setting
