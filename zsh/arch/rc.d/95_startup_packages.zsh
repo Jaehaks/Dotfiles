8d5b6b3d7695bff18885de6dd5d32309dc6bed7c
Jaehaks(arch) <wogkr3654@naver.com>
2025-07-22 08:20:08 +0900

    fix(zsh-arch) : add conditions to startup packages
    inspired from (z0rc dotfiles)[https://github.com/z0rc/dotfiles/blob/main/zsh/rc.d/00_tmux.zsh]

--- zsh/arch/rc.d/95_startup_packages.zsh
+++ zsh/arch/rc.d/95_startup_packages.zsh
@@ -1,6 +1,10 @@
 ## executes tmux at startup
-# -- check current session is out of tmux && current shell is interactive
-if [ -z "$TMUX" ] && [[ $- == *i* ]]; then
+# -- 1) check whether tmux command exists
+# -- 2) check current session is out of TMUX
+# -- 3) check current session is not in SSH remote access state
+# -- 4) check current user is not root
+# -- 5) check current shell is interactive
+if (( ${+commands[tmux]} )) && [[ ! -v TMUX && ! -v SSH_TTY && ${EUID} != 0 && $- == *i* ]]; then
 	# attach tmux if it is exists or start new session
 	tmux attach-session -t main 2>/dev/null || tmux new-session -s main
 fi
