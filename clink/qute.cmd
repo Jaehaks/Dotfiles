77acf2b61590f7470e37b0fee0b85290b9be3e33
Jaehaks <wogkr3654@naver.com>
2025-08-03 07:50:41 +0900

    fix(clink) : change qute.cmd to don't show cmd window
    with start command, cmd window to start qutebrowser don't  be shown
    but if you change target section in shorcut tab, cmd window will show again

--- clink/qute.cmd
+++ clink/qute.cmd
@@ -1,3 +1,9 @@
 @echo off
 
-qutebrowser --basedir %HOME%\.config\Dotfiles\qutebrowser
+REM To make a shortcut for this file,
+REM 1) right click to this file
+REM 2) open shortcut tab
+REM 3) modify "target(대상)" [from C:/~/qute.cmd] to [cmd /c "C:/~/qute.cmd"]
+REM 4) change icon as what you want
+REM 5) right click and register to taskbar(작업표시줄)
+start "" "qutebrowser" --basedir %HOME%\.config\Dotfiles\qutebrowser %*
