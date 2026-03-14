545821981ef008eaaf7d9b3f15d78c261fb2060c
Jaehaks <wogkr3654@naver.com>
2026-03-15 00:57:39 +0900

    feat(clink-prompt) : implement asynchronous behavior for git status prompt
    1. Problem
    
    If git status operation takes too long time, the prompt stocks without
    prompt.
    
    2. Solution
    
    To solve it, implement asynchronous behavior for git status checking
    process.
    Using promptcoroutine() and popenyield() of clink, check git status of
    current git repo asynchronously. If the asynchronous check is completed,
    redraw prompt.
    
    But If current directory is not same with git checking repo, don't show
    the git status

--- clink/clink_libs/prompt.lua
+++ clink/clink_libs/prompt.lua
@@ -63,110 +63,148 @@ end
 -- A prompt filter that appends the current git branch.
 -- local last_fetch_time = 0
 -- local wait_fetch_time = 10 * 60 -- 10 min
+---@class git_state
+---@field check_dir string directory where git checking process is started
+---@field prompt string prompt includes only git status
+---@field running boolean Flag whether git status checking is running
+local git_state = {
+	check_dir = "",
+	prompt = "",
+	running = false,
+}
 local git_branch_prompt = clink.promptfilter(65)
 function git_branch_prompt:filter(prompt)
-	local status = {
-		branch          = nil,
-		oid             = nil,
-		-- after modified
-		-- A, R, C mark cannot be shown in status before the files are added
-		workingModified = 0, --  M
-		workingDeleted  = 0, --  D
-		-- after add
-		stagingAdded    = 0, -- A(new untracked file), M(tracked file)
-		stagingRenamed  = 0, -- R
-		stagingCopied   = 0, -- C
-		stagingDeleted  = 0, -- D
-		stagingConflict = 0, -- U(merge conflict)
-		-- etc
-		untracked       = 0, -- ??
-		ignored         = 0, -- !!
-		stash           = 0, -- stash
-		commit          = 0, -- committed (remote hash < local hash)
-		behind          = 0, -- behind (remote hash > local hash)
-	}
 
-	-- get information of remote repo to gather `behind` as result of `git status`
-	-- INFO: git fetch is too heavy load operation because it use network.
-	-- INFO: To reduce the load at every prompt, wait time is set to reduce calling frequency
-	-- `--all` : fetch from all registered remote repo
-	-- `--prune` : remove local tracked status which don't exist in remote repo
-	-- local current_time = os.time()
-	-- if current_time - last_fetch_time > wait_fetch_time then
-	-- 	os.execute('git fetch --all --prune 2>nul 1>nul')
-	-- 	last_fetch_time = current_time
-	-- end
+	-- if directory is moved before git status checking is completed,
+	local curdir = os.getcwd()
+	if curdir == git_state.check_dir then
+		return prompt .. git_state.prompt
+	end
 
-	-- check status
-	for line in io.popen('git status --porcelain=v2 --branch --ignored=matching --show-stash 2>nul'):lines() do
-		-- get commit hash
-		local oid = line:match('^# branch%.oid%s+(.......)')
-		if oid then status.oid = oid end
+	-- if git checking is running, the result is not shown.
+	if git_state.running then
+		return prompt
+	end
 
-		-- get branch
-		local branch = line:match('^# branch%.head%s+(.+)')
-		if branch then
-			if branch == '(detached)' then
-				status.branch = status.oid
-			else
-				status.branch = branch
+	-- initialize
+	git_state.running = true
+	git_state.prompt = ""
+
+	-- git check using synchronous method
+	clink.promptcoroutine(function ()
+		git_state.check_dir = os.getcwd()
+		local status = {
+			branch          = nil,
+			oid             = nil,
+			-- after modified
+			-- A, R, C mark cannot be shown in status before the files are added
+			workingModified = 0, --  M
+			workingDeleted  = 0, --  D
+			-- after add
+			stagingAdded    = 0, -- A(new untracked file), M(tracked file)
+			stagingRenamed  = 0, -- R
+			stagingCopied   = 0, -- C
+			stagingDeleted  = 0, -- D
+			stagingConflict = 0, -- U(merge conflict)
+			-- etc
+			untracked       = 0, -- ??
+			ignored         = 0, -- !!
+			stash           = 0, -- stash
+			commit          = 0, -- committed (remote hash < local hash)
+			behind          = 0, -- behind (remote hash > local hash)
+		}
+
+		-- get information of remote repo to gather `behind` as result of `git status`
+		-- INFO: git fetch is too heavy load operation because it use network.
+		-- INFO: To reduce the load at every prompt, wait time is set to reduce calling frequency
+		-- `--all` : fetch from all registered remote repo
+		-- `--prune` : remove local tracked status which don't exist in remote repo
+		-- local current_time = os.time()
+		-- if current_time - last_fetch_time > wait_fetch_time then
+		-- 	os.execute('git fetch --all --prune 2>nul 1>nul')
+		-- 	last_fetch_time = current_time
+		-- end
+
+
+		-- check status
+		-- use popenyeild() to asynchronous operation in coroutine
+		local pipe = io.popenyield('git status --porcelain=v2 --branch --ignored=matching --show-stash 2>nul')
+		if not pipe then
+			return
+		end
+		for line in pipe:lines() do
+			-- get commit hash
+			local oid = line:match('^# branch%.oid%s+(.......)')
+			if oid then status.oid = oid end
+
+			-- get branch
+			local branch = line:match('^# branch%.head%s+(.+)')
+			if branch then
+				if branch == '(detached)' then
+					status.branch = status.oid
+				else
+					status.branch = branch
+				end
 			end
+
+			-- get number of commit
+			local commit, behind = line:match("^# branch%.ab%s+%+(%d+)%s+%-(%d+)")
+			if commit and behind then
+				status.commit = tonumber(commit)
+				status.behind = tonumber(behind)
+			end
+
+			-- get stash
+			local stash = line:match("^# stash%s+(%d+)")
+			if stash then
+				status.stash = tonumber(stash)
+			end
+
+			-- get status
+			if line:match("^1 .M(.+)$") then status.workingModified = status.workingModified + 1 end
+			if line:match("^1 .D(.+)$") then status.workingDeleted  = status.workingDeleted + 1 end
+
+			if line:match("^1 A(.+)$") then status.stagingAdded     = status.stagingAdded + 1 end
+			if line:match("^1 M(.+)$") then status.stagingAdded     = status.stagingAdded + 1 end
+			if line:match("^2 R(.+)$") then status.stagingRenamed   = status.stagingRenamed + 1 end
+			if line:match("^2 C(.+)$") then status.stagingCopied    = status.stagingCopied + 1 end
+			if line:match("^1 D(.+)$") then status.stagingDeleted   = status.stagingDeleted + 1 end
+			if line:match("^u%w%w (.+)$") then status.stagingConflict   = status.stagingConflict + 1 end
+
+			if line:match("^%?(.+)$") then status.untracked         = status.untracked + 1 end
+			-- if line:match("^%!(.+)$") then status.ignored           = status.ignored + 1 end
 		end
+		pipe:close()
 
-		-- get number of commit
-		local commit, behind = line:match("^# branch%.ab%s+%+(%d+)%s+%-(%d+)")
-		if commit and behind then
-			status.commit = tonumber(commit)
-			status.behind = tonumber(behind)
+		if status.branch then
+			local git_status_branch = git_status(status, 'branch')
+			local git_status_working = string.format('%s%s%s%s%s',
+										git_status(status, 'workingModified'),
+										git_status(status, 'workingDeleted'),
+										git_status(status, 'untracked'),
+										git_status(status, 'ignored'),
+										git_status(status, 'stash'))
+			local git_status_staging = string.format('\x1b[0m| %s%s%s%s%s%s%s',
+										git_status(status, 'stagingAdded'),
+										git_status(status, 'stagingRenamed'),
+										git_status(status, 'stagingCopied'),
+										git_status(status, 'stagingDeleted'),
+										git_status(status, 'stagingConflict'),
+										git_status(status, 'commit'),
+										git_status(status, 'behind'))
+
+			if (status.stagingAdded + status.stagingRenamed + status.stagingCopied + status.stagingDeleted + status.stagingConflict + status.commit + status.behind) == 0 then
+				git_state.prompt = ' ' .. git_status_branch .. git_status_working .. normal
+			else
+				git_state.prompt = ' ' .. git_status_branch .. git_status_working .. git_status_staging .. normal
+			end
+			clink.refilterprompt() -- redraw prompt. it calls all prompt filter again after coroutine is finished.
 		end
+		git_state.running = false
+	end)
 
-		-- get stash
-		local stash = line:match("^# stash%s+(%d+)")
-		if stash then
-			status.stash = tonumber(stash)
-		end
-
-		-- get status
-        if line:match("^1 .M(.+)$") then status.workingModified = status.workingModified + 1 end
-        if line:match("^1 .D(.+)$") then status.workingDeleted  = status.workingDeleted + 1 end
-
-		if line:match("^1 A(.+)$") then status.stagingAdded     = status.stagingAdded + 1 end
-        if line:match("^1 M(.+)$") then status.stagingAdded     = status.stagingAdded + 1 end
-        if line:match("^2 R(.+)$") then status.stagingRenamed   = status.stagingRenamed + 1 end
-        if line:match("^2 C(.+)$") then status.stagingCopied    = status.stagingCopied + 1 end
-        if line:match("^1 D(.+)$") then status.stagingDeleted   = status.stagingDeleted + 1 end
-        if line:match("^u%w%w (.+)$") then status.stagingConflict   = status.stagingConflict + 1 end
-
-        if line:match("^%?(.+)$") then status.untracked         = status.untracked + 1 end
-        -- if line:match("^%!(.+)$") then status.ignored           = status.ignored + 1 end
-
-	end
-
-	if status.branch then
-		local git_status_branch = git_status(status, 'branch')
-		local git_status_working = string.format('%s%s%s%s%s',
-									git_status(status, 'workingModified'),
-									git_status(status, 'workingDeleted'),
-									git_status(status, 'untracked'),
-									git_status(status, 'ignored'),
-									git_status(status, 'stash'))
-		local git_status_staging = string.format('\x1b[0m| %s%s%s%s%s%s%s',
-									git_status(status, 'stagingAdded'),
-									git_status(status, 'stagingRenamed'),
-									git_status(status, 'stagingCopied'),
-									git_status(status, 'stagingDeleted'),
-									git_status(status, 'stagingConflict'),
-									git_status(status, 'commit'),
-									git_status(status, 'behind'))
-
-		if (status.stagingAdded + status.stagingRenamed + status.stagingCopied + status.stagingDeleted + status.stagingConflict + status.commit + status.behind) == 0 then
-			return prompt .. ' ' .. git_status_branch .. git_status_working .. normal
-		else
-			return prompt .. ' ' .. git_status_branch .. git_status_working .. git_status_staging .. normal
-		end
-	end
-
-
+	-- show empty prompt while waiting git checking result
+	return prompt
 end
 
 -- A prompt filter that show python virtual env name
