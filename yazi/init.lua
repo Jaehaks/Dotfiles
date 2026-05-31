2772d6fdf71bd01755cce7fceee0a0d28fb7f1cb
Jaehaks <wogkr3654@naver.com>
2026-05-31 23:05:45 +0900

    fix : migration to v26.5.6
    1) git.yazi set its marks in theme.toml
    2) now $schema doesn't be used
    3) Synchronized with default config setting. it make be nice to compare
       updated config. theme.toml has more updated items for icons
    4) deprecated items are removed

--- yazi/init.lua
+++ yazi/init.lua
@@ -69,13 +69,6 @@ require('githead'):setup()
 
 -- [plugins] - yazi-rs/git.yazi
 -- show git status right after directory
-th.git = th.git or {}
-th.git.modified_sign  = "M"
-th.git.deleted_sign   = "D"
-th.git.added_sign     = "A"
-th.git.untracked_sign = "?"
-th.git.ignored_sign   = "-"
-th.git.updated_sign   = "U"
 require('git'):setup({
 	order = 500 -- order to show directory list. if 1500, gitsign go to rightmost
 })
